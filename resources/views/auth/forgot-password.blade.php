ac5be6a7a601212b08c6de3560ab3e0e4492a406
4nh31 <angelyupitxd@gmail.com>
2024-12-08 19:33:48 -0500

    le di estilo al login,quite logo laravel

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Olvidaste tu contraseña?? No hay problema. Solo dejanos saber tu direccion de correo electronico y te mandaremos un link para reestablecer.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Enlace para resstablecer contraseña') }}
             </x-primary-button>
         </div>
     </form>
