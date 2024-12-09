e880d4434bf2be3c96574dd378dd206b943efa74
GuillermoSM33 <guillermo.jesus.garcia.canul@gmail.com>
2024-12-09 18:00:56 -0500

    hotfix permission

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,11 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
-
 
 class PermissionSeeder extends Seeder
 {
@@ -17,21 +15,25 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-            $permissions = [
-                'ver',
-                'crear',
-                'editar',
-                'eliminar',
-            ];
-    
-            foreach ($permissions as $permission) {
-                Permission::firstOrCreate(['name' => $permission, 'guard_name' => 'web']);
-            }
-    
-            $adminRole = Role::firstOrCreate(['name' => 'Administrador', 'guard_name' => 'web']);
-            $adminRole->givePermissionTo($permissions);
-    
-            Role::firstOrCreate(['name' => 'Cajero', 'guard_name' => 'web']);
-        
+        $permissions = [
+            'ver',
+            'crear',
+            'editar',
+            'eliminar',
+        ];
+
+        $permissionsChasier = [
+            'ver',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission, 'guard_name' => 'web']);
+        }
+
+        $adminRole = Role::firstOrCreate(['name' => 'Administrador', 'guard_name' => 'web']);
+        $adminRole->givePermissionTo($permissions);
+
+        $chasierRole = Role::firstOrCreate(['name' => 'Cajero', 'guard_name' => 'web']);
+        $chasierRole->givePermissionTo($permissionsChasier);
     }
 }
