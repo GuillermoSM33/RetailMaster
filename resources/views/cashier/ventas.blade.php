ab672821dab7c781db8d17a72abceae57440d291
4nh31 <angelyupit1@outlook.es>
2024-12-10 05:21:09 -0500

    tabla dinamica, falta total

--- resources/views/cashier/ventas.blade.php
+++ resources/views/cashier/ventas.blade.php
@@ -8,90 +8,66 @@
 
 @section('contenido')
 <div class="custom-container">
-        <h1>Ventas</h1>
-        <div class="custom-search-bar">
-            <input type="text" id="search" placeholder="Código de producto o nombre de producto">
-            <div id="search-results" class="search-results"></div>
-            <button id="add-product">Agregar producto</button>
-            <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Generar Corte</button>
-        </div>
-        <div class="custom-payment-buttons">
-            <button class="custom-cash">
-                EFECTIVO
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                    <path d="M12 7.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
-                    <path fill-rule="evenodd" d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 14.625v-9.75ZM8.25 9.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM18.75 9a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V9.75a.75.75 0 0 0-.75-.75h-.008ZM4.5 9.75A.75.75 0 0 1 5.25 9h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H5.25a.75.75 0 0 1-.75-.75V9.75Z" clip-rule="evenodd" />
-                    <path d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z" />
-                </svg>
+    <h1>Ventas</h1>
 
-            </button>
-            <button class="custom-card">
-                TARJETA
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                    <path d="M4.5 3.75a3 3 0 0 0-3 3v.75h21v-.75a3 3 0 0 0-3-3h-15Z" />
-                    <path fill-rule="evenodd" d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clip-rule="evenodd" />
-                </svg>
-
-            </button>
-        </div>
-    </div>
-    
-
-    <!--TABLA-->
-    <div class="tabla">
-        <div class="total">
-            <span>Total: $ 1,234.00 MX</span>
-        </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Código producto</th>
-                    <th>Nombre producto</th>
-                    <th>Cantidad</th>
-                    <th>Precio</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>645353</td>
-                    <td>Pantalón Jeans</td>
-                    <td>3</td>
-                    <td>$ 175.00</td>
-                    <td class="actions">
-
-                        <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="#ff5555" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm3 10.5a.75.75 0 0 0 0-1.5H9a.75.75 0 0 0 0 1.5h6Z" clip-rule="evenodd" />
-                        </svg>
-                        </button>
-                        
-                        <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="#50fa7b" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
-                        </svg>
-                        </button>
-
-                        <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="#ff5555" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                            <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
-                        </svg>
-                        </button>
-                        
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <!-- Barra de búsqueda -->
+    <div class="custom-search-bar">
+        <input type="text" id="search" placeholder="Código de producto o nombre de producto">
+        <div id="search-results" class="search-results"></div>
+        <button id="add-product">Agregar producto</button>
+        <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Generar Corte</button>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- Botones de pago -->
+    <div class="custom-payment-buttons">
+        <button class="custom-cash">
+            EFECTIVO
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <path d="M12 7.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
+                <path fill-rule="evenodd" d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 14.625v-9.75ZM8.25 9.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0Z" clip-rule="evenodd" />
+                <path d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z" />
+            </svg>
+        </button>
+
+        <button class="custom-card">
+            TARJETA
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <path d="M4.5 3.75a3 3 0 0 0-3 3v.75h21v-.75a3 3 0 0 0-3-3h-15Z" />
+                <path fill-rule="evenodd" d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+            </svg>
+        </button>
+    </div>
+</div>
+
+<!-- Tabla de ventas -->
+<div class="tabla">
+    <div class="total">
+        <span>Total: $0.00 MX</span>
+    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>Código producto</th>
+                <th>Nombre producto</th>
+                <th>Cantidad</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script>
-document.getElementById('search').addEventListener('input', function() {
+let total = 0;
+
+document.getElementById('search').addEventListener('input', function () {
     const query = this.value;
     const resultsContainer = document.getElementById('search-results');
 
     if (query.length === 0) {
-        resultsContainer.innerHTML = ''; // Limpiar resultados si el input está vacío
+        resultsContainer.innerHTML = '';
         return;
     }
 
@@ -108,27 +84,89 @@ document.getElementById('search').addEventListener('input', function() {
             productos.forEach(producto => {
                 const item = document.createElement('div');
                 item.className = 'result-item';
-                item.textContent = `${producto.descripcion}`;
-                
-                // Asignar un data-id al item para almacenar el ID
-                item.setAttribute('data-id', producto.id_producto);
-                
-                // Agregar el item a los resultados
-                resultsContainer.appendChild(item);
-                
-                // Manejar el doble clic para poner el ID en el input
-                item.addEventListener('dblclick', function() {
-                    document.getElementById('search').value = item.getAttribute('data-id');
-                    resultsContainer.innerHTML = ''; // Limpiar los resultados después de seleccionar
+                item.textContent = `${producto.descripcion} - $${producto.precio_venta}`;
+                item.dataset.id = producto.id_producto;
+                item.dataset.precio = producto.precio_venta;
+
+                item.addEventListener('dblclick', function () {
+                    agregarProductoATabla(
+                        producto.id_producto,
+                        producto.descripcion,
+                        parseFloat(producto.precio_venta)
+                    );
+                    resultsContainer.innerHTML = '';
+                    document.getElementById('search').value = '';
                 });
+
+                resultsContainer.appendChild(item);
             });
         })
-        .catch(error => {
-            console.error('Error al buscar productos:', error);
-        });    
+        .catch(error => console.error('Error al buscar productos:', error));
 });
 
+function agregarProductoATabla(id, descripcion, precio) {
+    const tabla = document.querySelector('table tbody');
+    const fila = document.createElement('tr');
+
+    fila.innerHTML = `
+                <td>${id}</td>
+        <td>${descripcion}</td>
+        <td class="cantidad">1</td>
+        <td class="precio">$${precio.toFixed(2)}</td>
+        <td>
+            <button class="bajar-producto">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="#ff5555" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm3 10.5a.75.75 0 0 0 0-1.5H9a.75.75 0 0 0 0 1.5h6Z" clip-rule="evenodd" />
+                        </svg>
+             </button>
+
+            <button class="subir-producto">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#50fa7b" viewBox="0 0 24 24" class="size-6">
+                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
+                </svg>
+            </button>
+
+            <button class="eliminar-producto">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#ff5555" viewBox="0 0 24 24" class="size-6">
+                    <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </td>
+    `;
 
 
+    tabla.appendChild(fila);
+
+    fila.querySelector('.bajar-producto').addEventListener('click', () => actualizarCantidad(fila, -1, precio));
+    fila.querySelector('.subir-producto').addEventListener('click', () => actualizarCantidad(fila, 1, precio));
+    fila.querySelector('.eliminar-producto').addEventListener('click', () => eliminarProducto(fila, precio));
+
+    total += precio;
+    actualizarTotal();
+}
+
+function actualizarCantidad(fila, cambio, precio) {
+    const cantidadElem = fila.querySelector('.cantidad');
+    let cantidad = parseInt(cantidadElem.textContent);
+
+    cantidad += cambio;
+    if (cantidad <= 0) {
+        eliminarProducto(fila, precio);
+    } else {
+        cantidadElem.textContent = cantidad;
+        actualizarTotal();
+    }
+}
+
+function eliminarProducto(fila, precio) {
+    const cantidad = parseInt(fila.querySelector('.cantidad').textContent);
+    total -= precio * cantidad;
+    fila.remove();
+    actualizarTotal();
+}
+
+function actualizarTotal() {
+    document.querySelector('.total span').textContent = `Total: $${total.toFixed(2)} MX`;
+}
 </script>
 @endsection
